d4ac5aa7bf9a396e4513b2f336dd317c8274f98d
David Ramirez <davirami88@gmail.com>
2024-03-05 08:43:10 -0500

    Minor changes on return response

--- query_response.py
+++ query_response.py
@@ -2,6 +2,7 @@ import re
 import os
 import json
 import boto3
+import jsonify
 import unidecode
 import numpy as np
 import pandas as pd
@@ -17,6 +18,7 @@ from tenacity import (
 )  # for exponential backoff
 
 s3 = boto3.client("s3")
+s3_text = boto3.session.Session().resource('s3')
 
 embedding_engine = "text-embedding-ada-002"
 GPT3_turbo = "gpt-3.5-turbo-1106"
@@ -66,40 +68,78 @@ def get_openai_response(prompt, text, model):
 
     return result
 
-def get_query_response(query):
-    text_ranking = order_document_sections_by_query_similarity_openai(query, df_embddng)[:25]
+def get_query_response(s3_bucket_name, key_txt_pages, key_csv_text_file, key_csv_embeddings_file, query):
+    # Get the files from the S3 object
+    document_name = key_txt_pages
 
-    separator = "\n\n------\n\n"  # Clear visual separator
-    check_query = """Based on the text provided, you are to determine if it is possible to answer this question: """ + query + """
-    Please answer "yes" only if the answer to the question is in the text explicitly, clearly and unambiguously, and if the text contains the specific information to answer the question.
-    If is not possible to answer based on the text or does not directly and clearly identify the answer, or if there is any ambiguity or assumption required to identify them, you must answer "no".
-    Return your answer on the key "answer".
-    """
+    try:
+        res = s3_text.Object(s3_bucket_name, document_name)
 
-    prompt = "Based on the text, which a selected part of a whole documentation, return a JSON with a single answer in the key 'answer'. You must not recommedn read the docs, beacause the the text is an specific part of the whole documentation. Try to be as generic as possible and always based your answer on the text provided. Question: " + query
+        file_content = res.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
 
-    valid_text_options = []
+        text = []
+        for page in json_content:
+            text.append(page[0])
 
-    for option in text_ranking:
-        text_index = int(re.findall(r'\d+', str(option[1]))[0])
-        possible_text = text[text.find(df_txt.loc[text_index, "content"][:30]) - 500:text.find(df_txt.loc[text_index, "content"][:30]) + 2500]
-
-        response = get_openai_response(check_query, possible_text + separator, GPT3_turbo)
-
-        if response['answer'] == 'yes':
-            valid_text_options.append(possible_text)
-
-    if len(valid_text_options) > 0:
-        valid_text = " ".join(valid_text_options)
+        text = ' '.join(text)
         
-        response_user_query = get_openai_response(prompt, valid_text + separator, GPT3_turbo)
+        obj_text = s3.get_object(Bucket=s3_bucket_name, Key=key_csv_text_file)
+        obj_embeddings = s3.get_object(Bucket=s3_bucket_name, Key=key_csv_embeddings_file)
 
-        return response_user_query['answer']
-    else:
-        return "error"
+        # Read the object's content into pandas DataFrame
+        df_text = pd.read_csv(BytesIO(obj_text['Body'].read()))
+        df_embeddings = pd.read_csv(BytesIO(obj_embeddings['Body'].read()))
+        
+        text_ranking = order_document_sections_by_query_similarity_openai(query, df_embeddings)[:25]
+
+        separator = "\n\n------\n\n"  # Clear visual separator
+        prompt_check_query = """Based on the text provided, you are to determine if it is possible to answer this question: """ + query + """
+        Please answer "yes" only if the answer to the question is in the text explicitly, clearly and unambiguously, and if the text contains the specific information to answer the question.
+        If is not possible to answer based on the text or does not directly and clearly identify the answer, or if there is any ambiguity or assumption required to identify them, you must answer "no".
+        Return your answer on the key "answer".
+        """
+
+        prompt = "Based on the text, which a selected part of a whole documentation, return a JSON with a single answer in the key 'answer'. You must not recommedn read the docs, beacause the the text is an specific part of the whole documentation. Try to be as generic as possible and always based your answer on the text provided. Question: " + query
+
+        valid_text_options = []
+
+        for option in text_ranking:
+            text_index = int(re.findall(r'\d+', str(option[1]))[0])
+            possible_text = text[text.find(df_text.loc[text_index, "content"][:30]) - 500:text.find(df_text.loc[text_index, "content"][:30]) + 2500]
+
+            response = get_openai_response(prompt_check_query, possible_text + separator, GPT3_turbo)
+
+            if response['answer'] == 'yes':
+                valid_text_options.append(possible_text)
+
+        if len(valid_text_options) > 0:
+            valid_text = " ".join(valid_text_options)
+            
+            response_user_query = get_openai_response(prompt, valid_text + separator, GPT3_turbo)
+
+            return response_user_query['answer']
+        else:
+            return "openai_error"
+    except:
+        return "s3_download_error"
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     region = "us-east-1"
 
-    #TODO the code to read the "event" to get the embedding files and the user query, and also, run the functions get_query_response
+    try:
+        bucket = event["origin_bucket"]
+        key_txt_pages = event['txt_pages']
+        key_csv_text_file = event['csv_text_file']
+        key_csv_embeddings_file = event['csv_embeddings_file']
+        query = event['query']
+
+        response = get_query_response(bucket, key_txt_pages, key_csv_text_file, key_csv_embeddings_file, query)
+
+        payload = {"payload": response }
+
+        return jsonify({"message": payload}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
